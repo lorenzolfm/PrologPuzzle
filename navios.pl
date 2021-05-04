c463cc9812dbd40cc9597bb527b0b9def0f40224
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-05-04 07:45:28 -0300

    resolvendo bug

--- navios.pl
+++ navios.pl
@@ -28,11 +28,20 @@ destino(manila).
 destino(santos).
 destino(rotterdam).
 
-% X está à esquerda de Y (em qualquer posição à esquerda)
-aEsquerda(X, Y, Lista) :-
-  nth0(IndexX, Lista, X),
-  nth0(IndexY, Lista Y),
-  IndexX < IndexY
 
-% X está à direita de Y, se Y está a esquerda de X
-aDireita(X, Y, Lista) :- aEsquerda(Y, X, Lista).
+% X está à esquerda de Y (em qualquer posição à esquerda).
+
+
+% X está à direita de Y, se Y está a esquerda de X.
+
+/*
+% navio(Nacionalidade, Saida, Carregamento, Chamine, Destino)
+solucao(ListaSolucao) :-
+  ListaSolucao = [
+    navio(Nacionalidade1, Saida1, Carregamento1, Chamine1, Destino1),
+    navio(Nacionalidade2, Saida2, Carregamento2, Chamine2, Destino2),
+    navio(Nacionalidade3, Saida3, Carregamento3, Chamine3, Destino3),
+    navio(Nacionalidade4, Saida4, Carregamento4, Chamine4, Destino4),
+    navio(Nacionalidade5, Saida5, Carregamento5, Chamine5, Destino5).
+  ].
+*/
