6ad0134af5257dca1f4a5de0ea499eb1ec8e2988
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-05-04 08:44:17 -0300

    WIP: aEsquerda, aDireita e entre testados

--- main.pl
+++ main.pl
@@ -33,3 +33,32 @@ companhia(filho).
 companhia(irma).
 companhia(esposa).
 companhia(namorada).
+
+% X é o primeiro da Lista.
+primeiro(X, [X|_]).
+
+% X está à esquerda de Y (em qualquer posição à esquerda).
+aEsquerda(X, Y, Lista) :-
+  nth0(IndexX, Lista, X), nth0(IndexY, Lista, Y),
+  IndexX < IndexY.
+
+
+% X está à direita de Y, se Y está a esquerda de X.
+aDireita(X, Y, Lista) :- aEsquerda(Y, X, Lista).
+
+% X está em algum lugar entre X e Y.
+entre(X, Y, Z, Lista) :-
+   aDireita(X, Y, Lista),
+   aEsquerda(X, Z, Lista).
+
+/*
+% turista(Camisa, Nacionalidade, Bebida, Dias, Idade, Companhia)
+solucao(ListaSolucao) :-
+   ListaSolucao = [
+      turista(Camisa1, Nacionalidade1, Bebida1, Dias1, Idade1, Companhia1),
+      turista(Camisa2, Nacionalidade2, Bebida2, Dias2, Idade2, Companhia2),
+      turista(Camisa3, Nacionalidade3, Bebida3, Dias3, Idade3, Companhia3),
+      turista(Camisa4, Nacionalidade4, Bebida4, Dias4, Idade4, Companhia4),
+      turista(Camisa5, Nacionalidade5, Bebida5, Dias5, Idade5, Companhia5),
+   ]
+*/
